0e9cbcf5955ebe40464cb608686ad1393e0173c8
yuripourre <yuriamayuri@gmail.com>
2013-12-28 18:18:41 -0200

    LifeBar

--- Tide/src/quest/characters/Character.java
+++ Tide/src/quest/characters/Character.java
@@ -1,8 +1,11 @@
 package quest.characters;
 
+import java.awt.Color;
+
 import br.com.etyllica.core.video.Graphic;
 import br.com.etyllica.layer.AnimatedLayer;
 import br.com.etyllica.layer.StaticLayer;
+import br.com.etyllica.util.SVGColor;
 import br.com.tide.platform.player.Player;
 import br.com.tide.platform.player.PlayerState;
 
@@ -32,6 +35,19 @@ public class Character extends Player{
 	@Override
 	public void draw(Graphic g) {
 		layer.draw(g);
+		
+		drawLife(g);
+	}
+	
+	private void drawLife(Graphic g){
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(x, y+h+4, w, 4);
+		
+		int lifeW = (int)(((w-2)*health)/100);
+		
+		g.setColor(SVGColor.GREEN);
+		g.fillRect(x+1, y+h+5, lifeW, 2);
 	}
 
 	@Override
@@ -63,6 +79,8 @@ public class Character extends Player{
 	
 	@Override
 	public void onBeignHit() {
+		
+		health -= 5;
 			
 		layer.setFrames(3);
 		
--- Tide/src/quest/characters/enemy/Skeleton.java
+++ Tide/src/quest/characters/enemy/Skeleton.java
@@ -8,6 +8,8 @@ public class Skeleton extends Enemy{
 
 	public Skeleton(int x, int y, List<Player> players) {
 		super(x, y, "mv/skeleton.png", "mv/skeleton_inv.png", players);
+		
+		walkSpeed = 3;
 	}
 
 }
