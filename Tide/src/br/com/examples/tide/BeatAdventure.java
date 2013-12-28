1dc30a36eedc0c177e69fa728fd8ba5e98a8c0f9
Yuri Pourre <yuriamayuri@gmail.com>
2013-12-28 14:23:59 -0200

    New Images

--- Tide/src/br/com/examples/tide/BeatAdventure.java
+++ Tide/src/br/com/examples/tide/BeatAdventure.java
@@ -1,9 +1,14 @@
 package br.com.examples.tide;
 
 
-import quest.Adventurer;
-import quest.Hero;
-import quest.Ninja;
+import java.util.ArrayList;
+import java.util.List;
+
+import quest.characters.Adventurer;
+import quest.characters.Hero;
+import quest.characters.Ninja;
+import quest.characters.Satyr;
+import quest.characters.enemy.Skeleton;
 import br.com.etyllica.core.application.Application;
 import br.com.etyllica.core.event.GUIEvent;
 import br.com.etyllica.core.event.KeyEvent;
@@ -11,6 +16,7 @@ import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.video.Graphic;
 import br.com.tide.input.controller.EasyController;
 import br.com.tide.input.controller.FirstPlayerController;
+import br.com.tide.platform.player.Player;
 
 public class BeatAdventure extends Application{
 
@@ -18,9 +24,13 @@ public class BeatAdventure extends Application{
 		super(w, h);
 	}
 	
+	private Skeleton skeleton;
+	
 	private Ninja ninja;
 	
 	private Hero hero;
+	
+	private Satyr satyr;
 	
 	@Override
 	public void load() {
@@ -31,8 +41,17 @@ public class BeatAdventure extends Application{
 		ninja.setController(new EasyController());
 		
 		hero = new Adventurer(85, 250);
-		hero.setController(new FirstPlayerController());
+		//hero.setController(new FirstPlayerController());
 		
+		satyr = new Satyr(85, 200);
+		satyr.setController(new FirstPlayerController());
+		
+		List<Player> players = new ArrayList<Player>();
+		players.add(ninja);
+		players.add(satyr);
+		
+		skeleton = new Skeleton(200, 120, players);
+				
 		loading = 100;
 		
 		updateAtFixedRate(50);
@@ -44,12 +63,18 @@ public class BeatAdventure extends Application{
 	public void timeUpdate(){
 		ninja.update(0);
 		hero.update(0);
+		satyr.update(0);
+		
+		skeleton.update(0);		
 	}
 
 	@Override
 	public void draw(Graphic g) {
 		ninja.draw(g);
 		hero.draw(g);
+		
+		satyr.draw(g);
+		skeleton.draw(g);		
 	}
 	
 
@@ -63,10 +88,11 @@ public class BeatAdventure extends Application{
 	public GUIEvent updateKeyboard(KeyEvent event) {
 		
 		ninja.handleEvent(event);
-		hero.handleEvent(event);
+		//hero.handleEvent(event);
+		
+		satyr.handleEvent(event);
 		
 		return null;
 	}
-	
 
 }
